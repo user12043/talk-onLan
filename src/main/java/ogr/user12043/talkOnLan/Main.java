ca2c2042146f868a21a758482ba1c26b9928e696
user12043 <user12043@hotmail.com>
2022-06-12 22:42:12 +0300

    make mvn package jar-with-dependencies work

--- src/main/java/ogr/user12043/talkOnLan/Main.java
+++ src/main/java/ogr/user12043/talkOnLan/Main.java
@@ -1,39 +1,15 @@
 package ogr.user12043.talkOnLan;
 
-import ogr.user12043.talkOnLan.dao.DBConnection;
-import ogr.user12043.talkOnLan.ui.MainUI;
-import ogr.user12043.talkOnLan.util.Utils;
-
-import javax.swing.*;
 import java.util.logging.Logger;
 
 /**
- * @deprecated Created by user12043 on 23.07.2018 - 13:44
+ * Created by user12043 on 23.07.2018 - 13:44
  * part of project: talk-onLan
  */
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        try {
-            System.setProperty("java.net.preferIPv4Stack", "true"); // Set prefer to ipv4 addresses in java
-            // some property for smooth text
-            System.setProperty("swing.aatext", "true");
-            System.setProperty("awt.useSystemAAFontSettings", "on");
-
-            Utils.initDatabase();
-
-            DBConnection.get(); // init database connection
-            Utils.saveSelf();
-            Utils.saveSelfRoom();
-
-            if (!Utils.changeTheme("GTK+")) {
-                Utils.changeTheme("Windows");
-            }
-
-            SwingUtilities.invokeLater(() -> MainUI.getUI().setVisible(true)); // display main ui
-        } catch (Exception e) {
-            LOGGER.severe("Unexpected error happened!: \n" + e);
-        }
+        TalkOnLanApp.main(args);
     }
 }
