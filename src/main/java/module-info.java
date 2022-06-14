c518e57a73a896dc65f7e4950434e808a52ee931
user12043 <user12043@hotmail.com>
2022-06-14 19:56:21 +0300

    make javafx:jlink work (will need manual intervention for automatic modules)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,10 @@ module talkOnLan {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
-    requires org.xerial.sqlitejdbc; // "Error: automatic module cannot be used with jlink" for "mvn javafx:jlink"
-    // will not create application image because of the error above. Continue with the jar packaging bt "mvn package".
-    // Will generate custom jre with jlink manually
+//    requires org.xerial.sqlitejdbc; // "Error: automatic module cannot be used with jlink" for "mvn javafx:jlink"
+    // either
+    //      continue with the jar packaging bt "mvn package"
+    //      or add the sqlite to classpath after jlink generation. By adding -cp sqlite-jdbc.jar to the launcher
     opens ogr.user12043.talkOnLan;
     opens ogr.user12043.talkOnLan.controller to javafx.fxml;
 }
